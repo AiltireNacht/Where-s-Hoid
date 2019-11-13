f3a9815a1b509a14b24d2658f78da1e4cc554968
AiltireNacht <fnocte@yahoo.com>
2019-11-13 13:19:49 -0500

    Fixed Name. make it so n doesn't call the Order function

--- Wheres-Hoid.py
+++ Wheres-Hoid.py
@@ -6,30 +6,42 @@ def Intro():
 ( \/\/ )( )_( )( ___)(  _ \( ___) / __)  ( )_( )(  _  )(_  _)(  _ \ (__ )
  )    (  ) _ (  )__)  )   / )__)  \__ \   ) _ (  )(_)(  _)(_  )(_) ) (_/ 
 (__/\__)(_) (_)(____)(_)\_)(____) (___/  (_) (_)(_____)(____)(____/  (_) ''')
-    time.sleep(2)
-    def Order():
+
+    def Order(order):
+        #This defines what Order the RPG character plays as, defining what abilities are given and what ideals the player must play by
         ordr = input("\n\nYou are a member of the Knights Radiant.\nFrom what Order do you hail (Truthwatcher, Lightweaver, Windrunner)? ").lower().strip()
-        order = ordr
-        return order
-    time.sleep(2)
-    def Name():
-        n = Order()
-        
-        if n[0] == "w":
-            Order_Name = "Kaladin"
-        elif n[0] == "l":
-            Order_Name = "Shallan"
-        elif n[0] == "t":
-            Order_Name = "Renarin"
+        if ordr[0] == "w":
+            order = "Windrunner"
+        elif ordr[0] == "l":
+            order = "Lightweaver"
+        elif ordr[0] == "t":
+            order = "Truthwatcher"
         else:
-            print("Invalid Order Name. Please choose one of the three choices.")
-            Name()
-        nme = input("What is your name, Radiant? (Press enter to keep default name '" + Order_Name + "')")
-        name = nme
-        return name
+            print("\n\nInvalid Order Name. Please choose one of the three options")
+            Order("order")
+        return order
     
-    name = Name()
-    print(name, order)
+    def Name(name):
+        #This defines the name of the player. To be used for later reference
+        n = Order("order")
+        if n == "Windrunner":
+            Order_Name = "Kaladin"
+        elif n == "Lightweaver":
+            Order_Name = "Shallan"
+        else:
+            Order_Name = "Renarin"
+            
+        nme = input("What is your name, Radiant? (Press enter to keep default name '" + Order_Name + "')")
+
+        if nme == "": 
+            name = Order_Name
+        else:
+            name = nme
+        return name
+
+    order = "Windrunner" #Make it so order is one of the three options without having to repeat Order function
+    name = Name("name")
+    print(name, "of the Order of", order + "s")
 
 
     
